c08db27de1e443ecb26219fd9d813c1afac1abaf
Miguel Coder <syntacticsolutions@gmail.com>
2022-11-05 20:59:17 -0700

    Update database

--- database/migrations/1594761774801_votes.sql
+++ database/migrations/1594761774801_votes.sql
@@ -1,8 +1,13 @@
 CREATE TABLE IF NOT EXISTS {}.votes (
     author_id INT(12) NOT NULL,
     review_id INT(12) NOT NULL,
+    user_id INT(12) NOT NULL,
     liked TINYINT(1) NOT NULL,
     UNIQUE KEY(author_id, review_id),
+    CONSTRAINT fk_uvid FOREIGN KEY (user_id)
+    REFERENCES users(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
     CONSTRAINT fk_avid FOREIGN KEY (author_id)
     REFERENCES users(id)
     ON DELETE CASCADE
--- database/migrations/1594773979643_seeds.sql
+++ database/migrations/1594773979643_seeds.sql
@@ -17,10 +17,10 @@ VALUES
 (1, 3),
 (2, 1);
 
-INSERT INTO users (role_id, uid, email, first_name, last_name)
+INSERT INTO users (role_id, uid, email, first_name, last_name, personalQuote, profession, location)
 VALUES
-(1, 'f0a9s70a9s7df987asd','mcoder@lavishweb.com', 'Miguel', 'Coder'),
-(2, '0a98s7d0fa87s9d087','dummy@person.com', 'Dummy', 'Person');
+(1, 'f0a9s70a9s7df987asd','mcoder@lavishweb.com', 'Miguel', 'Coder', 'Humility is key', 'developer', 'San Francisco'),
+(2, '0a98s7d0fa87s9d087','dummy@person.com', 'Bob', 'Dylan', 'You\'re gonna have to serve somebody', 'musician', 'New York');
 
 INSERT INTO review_types (type, color)
 VALUES
@@ -34,11 +34,11 @@ VALUES
 (2, 2, 1, 5, 5, 4, 'Miguel is very punctual and likes to tackle the tough problems'),
 (3, 2, 1, 3, 5, 5, 'Miguel is a good friend and has a really dry sense of humor.');
 
-INSERT INTO votes (author_id, review_id, liked)
+INSERT INTO votes (author_id, review_id, liked, user_id)
 VALUES
-(1, 1, 1),
-(1, 2, 1),
-(1, 3, 0);
+(2, 1, 1, 1),
+(2, 2, 1, 1),
+(2, 3, 0, 1);
 
 INSERT INTO comments(author_id, review_id, text)
 VALUES
